c877f409c431c83de3d52f440d370efc5bf49bca
Nobumasa Kashiwagi <33705223+nobukash@users.noreply.github.com>
2019-12-19 14:56:03 +0900

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,10 +1,12 @@
 #########IMPORT LIBRARIES HERE IF NEEDED#######
-
+from statistics import median
 ################END IMPORTS####################
 """
 Write a function that takes a list of numbers as input and returns the median of the list.
 """
 def f(l):
     ##########YOUR CODE HERE##########
-    pass
+    l = [1, 2, 3, 4, 5]
+    median = median(l)
+    print("medan is".format(median))
     ###########END CODE###############
