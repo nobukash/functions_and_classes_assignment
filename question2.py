7d0085c129a7f8a97ac407092ebf7179e0b0c228
Nobumasa Kashiwagi <33705223+nobukash@users.noreply.github.com>
2019-12-19 14:19:16 +0900

    Update question2.py

--- question2.py
+++ question2.py
@@ -9,5 +9,17 @@ f([0, -4, 2, -3, 6, -1, 8, 6]) should return 22.
 
 def f(l):
     ##########YOUR CODE HERE##########
-    pass
+  b = []
+  for l in range(0,n):
+    b.append(l)
+  
+  l1=0
+  l2=0
+    
+  for l in b:
+    if(l>0):
+      if(l%2==0):
+        l1=l1+l
+      else:
+        l2 = l2+l
     ###########END CODE###############
